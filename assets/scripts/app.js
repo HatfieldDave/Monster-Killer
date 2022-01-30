fce74145644447bc2685691eb664d3ac96d66156
David Hatfield <davidhatfield@Davids-MacBook-Pro.local>
2022-01-29 23:42:13 -0500

    feat: added fuctionality to attack and strong attack btns

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -6,9 +8,36 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-	const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+	let maxDamage;
+	if (mode === 'ATTACK') {
+		maxDamage = ATTACK_VALUE;
+	} else if (mode === 'STRONG_ATTACK') {
+		maxDamage = STRONG_ATTACK_VALUE;
+	}
+	const damage = dealMonsterDamage(maxDamage);
 	currentMonsterHealth -= damage;
+	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+	currentPlayerHealth -= playerDamage;
+	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+		alert('You Win');
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+		alert('You lose');
+	} else if (currentPlayerHealth < 0 && currentMonsterHealth <= 0) {
+		alert('Draw');
+	}
 }
 
+function attackHandler() {
+	attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+	attackMonster('STRONG_ATTACK');
+}
+
+function healPlayerHandler() {}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
